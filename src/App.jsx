bad3432e0223200089bfb12bcacbf995c8917a7b
Dominik Bil <dominik.bil64@gmail.com>
2023-06-07 07:20:15 +0200

    Fetch API and resolve CORS

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,53 @@
-// import { useState } from 'react'
-import { useEffect } from 'react';
-import './App.css'
-import axios from 'axios';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  //const [count, setCount] = useState(0)
-
+  const [categories, setCategories] = useState(0);
+  const apiKey = "BDc4JU6zygD3NDiV31ifniVDHoCClu5c";
+  const url = "http://localhost:3000/api/categories";
+  const options = {
+    method: "GET",
+    headers: {
+      "API-Key": apiKey,
+    },    
+  };
 
   useEffect(() => {
-    axios.get('')
-    .then(res => {
-      console.log(res.data)
-    })
-  }, [])
-  
+    fetch(url, options)
+        .then((response) => (response.json()))
+        .then(data => setCategories(data))
+  }, []);
+
+console.log(categories)
+
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
-    <div className='page'>
+    <div className="page">
       <form onSubmit={handleSubmit}>
-
-      {/* Contact info */}
+        {/* Contact info */}
+        <div>{/* {categories} */}</div>
 
         <div>
           <label>Imie i nazwisko: </label>
-          <input id="contact_name" type="string" defaultValue={"imie"}/>
+          <input id="contact_name" type="string" defaultValue={"imie"} />
         </div>
         <div>
           <label>Telefon: </label>
-          <input id="telephone" type="integer" defaultValue={420213769}/>
+          <input id="telephone" type="integer" defaultValue={420213769} />
         </div>
 
-      {/* Basic Informations */}
+        {/* Basic Informations */}
 
         <div>
           <label>Glowna kategoria: </label>
-          <input id="cat1_id" type="number" defaultValue={1}/>
+          <input id="cat1_id" type="number" defaultValue={1} />
         </div>
         <div>
           <label>Podkategoria: </label>
-          <input id="cat2_id" type="number" defaultValue={2}/>
+          <input id="cat2_id" type="number" defaultValue={2} />
         </div>
         <div>
           <label>Kategoria trzeciego rzedu: </label>
@@ -56,15 +62,15 @@ function App() {
         </div>
         <div>
           <label>rodzaj ogloszenia</label>
-          <input id="ad_type" type="integer" defaultValue={1}/>
+          <input id="ad_type" type="integer" defaultValue={1} />
         </div>
         <div>
           <label>Tytul ogloszenia: </label>
-          <input id="title" type="string" min={5}/> 
+          <input id="title" type="string" min={5} />
         </div>
-        <div id='divDescription'>
-            <label id='labelDescription'>opis ogloszenia: </label>
-            <textarea id="description" max={4000}></textarea>
+        <div id="divDescription">
+          <label id="labelDescription">opis ogloszenia: </label>
+          <textarea id="description" max={4000}></textarea>
         </div>
         <div>
           <label>Cena</label>
@@ -73,30 +79,26 @@ function App() {
 
         <div>
           <label>Zdjecie: </label>
-          <input type='file'></input>
-        </div>  
-
+          <input type="file"></input>
+        </div>
 
         <button type="submit">Wyslij</button>
       </form>
     </div>
-    
-    
-    
-  )
+  );
 }
 
-export default App
+export default App;
 // <>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
+//   <div className="card">
+//     <button onClick={() => setCount((count) => count + 1)}>
+//       count is {count}
+//     </button>
+//     <p>
+//       Edit <code>src/App.jsx</code> and save to test HMR
+//     </p>
+//   </div>
+//   <p className="read-the-docs">
+//     Click on the Vite and React logos to learn more
+//   </p>
+// </>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.oglaszamy24.pl/',
+        changeOrigin: true,
+        //"API-Key": "BDc4JU6zygD3NDiV31ifniVDHoCClu5c",
+
+      }
+    }
+  }
 })
