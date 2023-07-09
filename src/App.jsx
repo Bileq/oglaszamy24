805cd926ec82cc838e4cc913224a4954480ec826
Dominik Bil <dominik.bil64@gmail.com>
2023-07-09 08:20:26 +0200

    Prepare form

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { useState, useEffect } from "react";
 function App() {
   const [mainCategory, setMainCategory] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false)
-  const [subCategory, setSubCategory] = useState(0);
+  const [subCategory, setSubCategory] = useState([]);
   const [thirdRowCategory, setThirdRowCategory] = useState(0);
 
   const apiKey = "BDc4JU6zygD3NDiV31ifniVDHoCClu5c";
-  const url = "http://localhost:3000/api/categories";
+  const urlCats = "http://localhost:3000/api/categories";
+  //const urlCities = "http://localhost:3000/api/cities"
   const options = {
     method: "GET",
     headers: {
@@ -18,10 +19,11 @@ function App() {
 
   useEffect(() => {
     const fetchAPI = async () => {
-      const res = await fetch(url, options);
+      const res = await fetch(urlCats, options);
       const data = await res.json();
-      // setMainCategory(Object.values(data.data));
-      setMainCategory(data.data)
+      // setMainCategory(data);
+      setMainCategory(data.data.categories["Nauka i książki"].cat1_id)
+      setSubCategory(data.data.categories["Nauka i książki"].subcategories["Książki i podręczniki"].cat2_id)
       setIsLoaded(true)
     };
 
@@ -31,26 +33,68 @@ function App() {
 
   // console.log(mainCategory.categories.Praca)
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   if (!isLoaded) {
     return <div>Loading...</div>
     
   } else {
-    console.log(mainCategory.categories)
+    console.log(mainCategory)
+    console.log(subCategory)
 
     return (
       <div className="page">
-        <ul>
-          {
-            Object.keys(mainCategory.categories).map(category => (
-            <li key={category}>
-             {category}
-            </li>
-           ))}
-         </ul>
+            <div className='page'>
+      <form onSubmit={handleSubmit}>
+
+      {/* Contact info */}
+
+        <div>
+          <label>Imie i nazwisko: </label>
+          <input id="contact_name" type="string" defaultValue={"imie"}/>
+        </div>
+        <div>
+          <label>Telefon: </label>
+          <input id="telephone" type="integer" defaultValue={420213769}/>
+        </div>
+
+      {/* Basic Informations */}
+
+        <div>
+          <label>Glowna kategoria: </label>
+          <input id="cat1_id" type="number" defaultValue={mainCategory}/>
+        </div>
+        <div>
+          <label>podkategoria: </label>
+          <input id="cat1_id" type="number" defaultValue={subCategory}/>
+        </div>
+        <div>
+          <label>miasto: </label>
+          <input id="city_id" type="number" defaultValue={14}/>
+        </div>
+        <div>
+          <label>rodzaj ogloszenia: </label>
+          <input id="ad_type" type="integer" defaultValue={1}/>
+        </div>
+        <div>
+          <label>Tytul ogloszenia: </label>
+          <input id="title" type="string" min={5}/> 
+        </div>
+        <div id='divDescription'>
+            <label id='labelDescription'>opis ogloszenia: </label>
+            <textarea id="description" max={4000}></textarea>
+        </div>
+        <div>
+          <label>Pierwsze zdjecie: </label>
+          <input type='file'></input>
+        </div>
+
+
+        <button type="submit">Wyslij</button>
+      </form>
+    </div>
        </div>
     );
   }
---
+++ src/unused.js
@@ -0,0 +1,41 @@
+
+// {
+//     "status": "ok",
+//     "data": {
+//         "categories": {
+//             "Motoryzacja": {
+//                 "cat1_id": "1"
+
+//             },
+//             "Nieruchomości": {
+//                 "cat1_id": "2"
+
+//             },
+//             "Praca": {
+//                 "cat1_id": "3"
+
+//             }
+//             }}}
+
+            {/* console.log(data[1].Motoryzacja.cat1_id) */}
+            {/* console.log(data[1].item) */}
+            
+            {/* return <li key={}>{data}</li>; */}
+            {/* //data
+            console.log("data: ")
+            console.log(data);
+            //categories
+            console.log("categories:")
+            console.log(data[1]);
+            console.log(data[1].Motoryzacja.subcategories.Samochody.subcategories)
+            console.log(Object.keys(data[1]))
+            console.log("===========================================================") */}
+
+        //     <ul>
+        //   {
+        //     Object.keys(mainCategory.categories).map(category => (
+        //     <li key={category}>
+        //      {category}
+        //     </li>
+        //    ))}
+        //  </ul>
