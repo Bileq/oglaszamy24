bce1243840f858901555aab610be000388efc4e1
Dominik Bil <dominik.bil64@gmail.com>
2023-07-10 20:16:31 +0200

    Handling adding adverts

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [mainCategory, setMainCategory] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [subCategory, setSubCategory] = useState([]);
-  const [thirdRowCategory, setThirdRowCategory] = useState(0);
+  const [mainCategory, appendMainCategory] = useState([]);
+  const [isLoaded, appendIsLoaded] = useState(false);
+  const [subCategory, appendSubCategory] = useState([]);
+  const [name, appendName] = useState("Jakub Siębor")
+  const [phone, appendPhone] = useState(123456789)
+  const [catOne, appendCatOne] = useState(13)
+  const [catTwo, appendCatTwo] = useState(80)
+  const [catThree, appendCatThree] = useState(0);
+  const [city, appendCity] = useState(14)
+  const [adType, appendAdType] = useState(1)
+  const [title, appendTitle] = useState("deleteLater")
+  const [desc, appendDesc] = useState("deleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLaterdeleteLater")
+  const [photo, appendPhoto] = useState([])
 
   const apiKey = "BDc4JU6zygD3NDiV31ifniVDHoCClu5c";
   const urlCats = "http://localhost:3000/api/categories";
-  //const urlCities = "http://localhost:3000/api/cities"
   const options = {
     method: "GET",
     headers: {
@@ -21,85 +29,171 @@ function App() {
     const fetchAPI = async () => {
       const res = await fetch(urlCats, options);
       const data = await res.json();
-      // setMainCategory(data);
-      setMainCategory(data.data.categories["Nauka i książki"].cat1_id)
-      setSubCategory(data.data.categories["Nauka i książki"].subcategories["Książki i podręczniki"].cat2_id)
-      setIsLoaded(true)
+      appendMainCategory(data.data.categories["Nauka i książki"].catOne);
+      appendSubCategory(
+        data.data.categories["Nauka i książki"].subcategories[
+          "Książki i podręczniki"
+        ].catTwo
+      );
+      appendIsLoaded(true);
     };
 
     fetchAPI().catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // console.log(mainCategory.categories.Praca)
-
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
+
+    const data = {
+        "title": title,
+        "description": desc,
+        "cat1_id": catOne,
+        "cat2_id": catTwo,
+        "cat3_id": catThree,
+        "contact_name": name,
+        "telephone": phone,
+        "city_id": city,
+        "ad_type": adType
+    }
+    
+    console.log([data])
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        "API-Key": apiKey, 
+        'Content-Type': 'application/x-www-form-urlencoded',
+
+       },
+       body: new URLSearchParams({ data: JSON.stringify(data) })
+      }
+
+    fetch('http://localhost:3000/api/adverts', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+    
   };
 
   if (!isLoaded) {
-    return <div>Loading...</div>
-    
+    return <div>Loading...</div>;
   } else {
-    console.log(mainCategory)
-    console.log(subCategory)
 
     return (
       <div className="page">
-            <div className='page'>
-      <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} id="form">
+          {/* Contact info */}
 
-      {/* Contact info */}
+          <div>
+            <label >Imie i nazwisko: </label>
+            <input
+              required
+              value={name} 
+              id="contact_name" 
+              type="string" 
+              onChange={e => appendName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Telefon: </label>
+            <input 
+              required
+              value={phone}
+              id="telephone" 
+              type="integer"
+              onChange={e => appendPhone(e.target.value)} 
+            />
+          </div>
 
-        <div>
-          <label>Imie i nazwisko: </label>
-          <input id="contact_name" type="string" defaultValue={"imie"}/>
-        </div>
-        <div>
-          <label>Telefon: </label>
-          <input id="telephone" type="integer" defaultValue={420213769}/>
-        </div>
+          {/* Basic Informations */}
 
-      {/* Basic Informations */}
+          <div>
+            <label>Glowna kategoria: </label>
+            <input 
+              required
+              value={catOne}
+              id="cat1_id" 
+              type="number" 
+              onChange={e => appendCatOne(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Podkategoria: </label>
+            <input 
+              required 
+              value={catTwo}
+              id="cat2_id" 
+              type="number"
+              onChange={e => appendCatTwo(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Kategoria trzeciego rzedu: </label>
+            <input 
+              required 
+              value={catThree}
+              id="cat3_id" 
+              type="number"
+              onChange={e => appendCatThree(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Miasto: </label>
+            <input 
+              value={city}
+              id="city_id" 
+              type="number" 
+              onChange={e => appendCity(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Rodzaj ogloszenia: </label>
+            <input
+              required 
+              value={adType} 
+              id="ad_type" 
+              type="integer" 
+              onChange={e => appendAdType(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="title">Tytul ogloszenia: </label>
+            <input 
+              required 
+              value={title}
+              id="title" 
+              type="string" 
+              min={5} 
+              onChange={e => appendTitle(e.target.value)}
+            />
+          </div>
+          <div id="divdesc">
+            <label id="labeldesc">Opis ogloszenia: </label>
+            <textarea 
+              required 
+              value={desc}
+              id="description" 
+              max={4000}
+              onChange={e => appendDesc(e.target.value)}
+              >
+              </textarea>
+          </div>
+          <div>
+            <label>Zdjecie: </label>
+            <input 
+              type="file"
+              value={photo}
+              onChange={e => appendPhoto(e.target.value)}
+            ></input>
+          </div>
 
-        <div>
-          <label>Glowna kategoria: </label>
-          <input id="cat1_id" type="number" defaultValue={mainCategory}/>
-        </div>
-        <div>
-          <label>podkategoria: </label>
-          <input id="cat1_id" type="number" defaultValue={subCategory}/>
-        </div>
-        <div>
-          <label>miasto: </label>
-          <input id="city_id" type="number" defaultValue={14}/>
-        </div>
-        <div>
-          <label>rodzaj ogloszenia: </label>
-          <input id="ad_type" type="integer" defaultValue={1}/>
-        </div>
-        <div>
-          <label>Tytul ogloszenia: </label>
-          <input id="title" type="string" min={5}/> 
-        </div>
-        <div id='divDescription'>
-            <label id='labelDescription'>opis ogloszenia: </label>
-            <textarea id="description" max={4000}></textarea>
-        </div>
-        <div>
-          <label>Pierwsze zdjecie: </label>
-          <input type='file'></input>
-        </div>
-
-
-        <button type="submit">Wyslij</button>
-      </form>
-    </div>
-       </div>
+          <button type="submit">Wyslij</button>
+        </form>
+      </div>
     );
   }
-
-  
 }
 
 export default App;
